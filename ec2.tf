6feadf31ea9ae39c9879679c2ce611a0814cc315
vrapracash <veeraprakash.bg@gmail.com>
2025-01-31 15:43:17 +0530

    scripts-update

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 module "ec2" {
-    source = "../terraform-aws-ec2"
+    source = "/home/veera/Documents/veera-devops/devops-practice/terraform-1/terraform-aws-ec2"
     # these are variables excepted modu;e, not arguments of resource definition
     # you can also create variables or else can directly use values here
     ami_id = var.ami_something
--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "public_ip {
-  value       = module.ec2.public_ip
+    value  = module.ec2.public_ip
 }
 
 output "private_ip" {
