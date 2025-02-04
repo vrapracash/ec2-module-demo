b7c56d910c2045fc643a3ebf7b22895e8b9353ba
vrapracash <veeraprakash.bg@gmail.com>
2025-02-04 10:10:07 +0530

    script-update

--- ec2.tf
+++ ec2.tf
@@ -3,6 +3,6 @@ module "ec2" {
     # these are variables excepted modu;e, not arguments of resource definition
     # you can also create variables or else can directly use values here
     ami_id = var.ami_something
-    instance_type = "t3.large"
+    instance_type = "t3.small"
     security_group_ids = ["sg-0bfc14741c51ad341"]
 }
